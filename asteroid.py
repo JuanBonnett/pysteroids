39071f17a520a0c7a44453828d9fbe502fcd911d
Blake O'Hare <mspaintmaestro@gmail.com>
2017-01-07 23:54:26 -0800

    Don't re-use duplicate position vector references when creating fragmented debris.

--- asteroid.py
+++ asteroid.py
@@ -81,7 +81,7 @@ class AsteroidGenerator(object) :
 			if _pos == None :
 				pos = Vector2D(random.randint(0, self.screen.get_width()), random.randint(0, self.screen.get_height() / 2))
 			else :
-				pos = _pos
+				pos = Vector2D(_pos.x, _pos.y)
 
 			asteroid = Asteroid(self.screen, pos, size, vert_count)
 			asteroid.set_velocity(Vector2D(random.randint(1, 3), random.randint(1, 3)))
